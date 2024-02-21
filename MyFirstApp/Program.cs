443bc59db18dc0c454d65c6bc3f76d8de4edbc8a
Karl Takker <karl.martin.takker@tktk.ee>
2024-02-22 00:33:39 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+
+//Console.WriteLine("Hello" + ", "+userName + "!");
+
+
+Console.WriteLine($"Hello, {userName}!");
